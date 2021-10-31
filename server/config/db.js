91294ee2a1d4eeb67e3ed9369d8382d478cf9521
Dino Pacariz <dinopacariz@gmail.com>
2021-10-31 09:27:37 +0100

    connected database

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const db = {
+  mongoose: mongoose,
+  url: process.env.DB_URI,
+  config: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+}
+
+module.exports = db;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,17 @@
+require('dotenv').config();
 const server = require('./app');
 const PORT = process.env.PORT || 5000;
+const db = require('./config/db');
+
+// Database
+db.mongoose
+.connect(db.url, db.config)
+.then(() => {
+  console.log('Connected to the database!');
+})
+.catch(err => {
+  console.log('Cannot connect to the database!', err);
+  process.exit();
+});
 
 server.listen(PORT);
