4ebfd98195af941e7722ad41d1edfe7b66f67d0b
Dino Pacariz <dinopacariz@gmail.com>
2021-11-04 13:51:05 +0100

    corrected date format on mockdata

--- client/src/App.js
+++ client/src/App.js
@@ -11,41 +11,42 @@ const todos = [
     id: 1,
     title: 'First todo',
     content: '..and it\'s content',
-    lastEditTime: Date.parse('04 Oct 2021 01:12:00 GMT'),
+    lastEditTime: new Date('04 Oct 2021 01:12:00 GMT'),
   },
   {
     id: 2,
     title: 'Thoughts',
     content: 'This and that',
-    lastEditTime: Date.parse('06 Oct 2021 05:24:32 GMT'),
+    lastEditTime: new Date('06 Oct 2021 05:24:32 GMT'),
   },
   {
     id: 3,
     title: 'Shopping List',
     content: 'potatoes\n\nbeans\n\nflour',
-    lastEditTime: Date.parse('07 Oct 2021 10:11:03 GMT'),
+    lastEditTime: new Date('07 Oct 2021 10:11:03 GMT'),
   },
   {
     id: 4,
     title: '',
     content: 'No title',
-    lastEditTime: Date.parse('08 Oct 2021 01:12:00 GMT'),
+    lastEditTime: new Date('08 Oct 2021 01:12:00 GMT'),
   },
   {
     id: 5,
     title: 'Earlier date than above, and no content',
     content: '',
-    lastEditTime: Date.parse('03 Oct 2021 00:03:00 GMT'),
+    lastEditTime: new Date('03 Oct 2021 00:03:00 GMT'),
   },
 ];
 
 export default function App() {
-  const [todoList, setTodoList] = useState([])
+  const [todoList, setTodoList] = useState([]);
 
   useEffect(() => {
+    // TODO get todos from db
     setTodoList(todos)
-  }, [])
-
+  }, []);
+  
   return (
     <>
       <UserContext.Provider value={{todoList}}>
@@ -56,12 +57,9 @@ export default function App() {
             <Route path='/todo/:id' component={TodoDetailsPage} />
             <Route path='/' component={HomePage} />
           
-          {/*
-          
-          //TODO 404 route without changing url 
+          {/* //TODO 404 route without changing url 
           https://github.com/remix-run/react-router/blob/v3/docs/guides/Histories.md#creatememoryhistory
-          
-        */}
+          */}
         </Switch>
       </UserContext.Provider>
     </>
