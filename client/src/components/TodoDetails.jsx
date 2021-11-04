30431a3e936054fc1cab3473c8631787a54ae373
Dino Pacariz <dinopacariz@gmail.com>
2021-11-04 13:52:11 +0100

    implemented back button in tododetails

--- client/src/components/TodoDetails.jsx
+++ client/src/components/TodoDetails.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useHistory } from "react-router-dom";
 import ReactMarkdown from 'react-markdown';
+import moment from 'moment';
+import BackBtn from './BackBtn';
 
 export default function TodoDetails({data}) {
   const { title, content, lastEditTime, id } = data;
@@ -15,6 +17,7 @@ export default function TodoDetails({data}) {
       <p>{title}</p>
       <ReactMarkdown>{content}</ReactMarkdown>
       <p>{`Last edited ${lastEditTime}`}</p>
+      <BackBtn label="Go Back" />
       <button type="button" onClick={openEdit}>Edit</button>
     </>
   )
