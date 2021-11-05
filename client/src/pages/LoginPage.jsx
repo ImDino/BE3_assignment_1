7f50be74874ee262bd2d8c3717c12ec31fa50684
Dino Pacariz <dinopacariz@gmail.com>
2021-11-05 15:27:40 +0100

    added post for google auth

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import GoogleLogin from 'react-google-login';
+import axios from 'axios';
 
 export default function LoginPage() {
   const responseSuccessGoogle = (res) => {
-    console.log(res);
+    const tokenId = res.tokenObj.id_token;
+    const serverRoot = process.env.REACT_APP_SERVER_ROOT;
+    const path = `${serverRoot}/auth/google`;
+    
+    axios.post(path, { tokenId: tokenId });
   }
   
   const responseErrorGoogle = (res) => {
@@ -19,7 +24,7 @@ export default function LoginPage() {
         onSuccess={responseSuccessGoogle}
         onFailure={responseErrorGoogle}
         cookiePolicy={'single_host_origin'}
-      />,
+      />
     </div>
   )
 }
