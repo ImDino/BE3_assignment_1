d75cedd4592546ffbb224e8c92cc40d5b9f57de0
Dino Pacariz <dinopacariz@gmail.com>
2021-11-05 15:27:20 +0100

    implemented cors

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
+const cors = require('cors');
 const express = require('express')
 const app = express()
 
+//Middleware
 const TodoRoute = require('./routes/TodoRoute');
 const AuthRoute = require('./routes/AuthRoute');
- 
+
+//Cors
+app.use(cors({
+  origin: 'http://localhost:3000',
+  //methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+  credentials: true,
+}));
+
+//Routes
 app.use('/', TodoRoute);
 app.use('/auth', AuthRoute);
 
