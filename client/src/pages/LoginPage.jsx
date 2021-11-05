d9c0a173aa6c7c65d1bd7222133625e581bbc7e4
Dino Pacariz <dinopacariz@gmail.com>
2021-11-05 14:43:26 +0100

    implemented react-dotenv, started working on google login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import GoogleLogin from 'react-google-login';
+import env from "react-dotenv";
 
 export default function LoginPage() {
   return (
     <div>
-      login
+      <h2>Login with google</h2>
+      <GoogleLogin
+        clientId={env.GOOGLE_CLIENT_ID}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />,
     </div>
   )
 }
