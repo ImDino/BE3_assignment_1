9c84c087cca8f626022287dcc2c4939cee3ebfcf
Dino Pacariz <dinopacariz@gmail.com>
2021-11-06 18:47:12 +0100

    added user route

--- server/app.js
+++ server/app.js
@@ -5,11 +5,11 @@ const app = express()
 //Middleware
 const TodoRoute = require('./routes/TodoRoute');
 const AuthRoute = require('./routes/AuthRoute');
+const UserRoute = require('./routes/UserRoute');
 
 //Cors
 app.use(cors({
   origin: 'http://localhost:3000',
-  //methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
   credentials: true,
 }));
 
@@ -18,5 +18,6 @@ app.use(express.json());
 //Routes
 app.use('/', TodoRoute);
 app.use('/auth', AuthRoute);
+app.use('/user', UserRoute);
 
 module.exports = app;
