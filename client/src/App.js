3615945c9437401d2e10fa62048ff9f0a243b593
Dino Pacariz <dinopacariz@gmail.com>
2021-11-06 18:46:03 +0100

    validation and message handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { Switch, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import { UserContext } from './contexts/UserContext';
 import TodoDetailsPage from './pages/TodoDetailsPage';
 import TodoEditPage from './pages/TodoEditPage';
+import { FetchKit, getToken } from './data/FetchKit';
 
+/* 
+NOTE mockdata example
 const todos = [
   {
     id: 1,
@@ -13,55 +16,66 @@ const todos = [
     content: '..and it\'s content',
     lastEditTime: new Date('04 Oct 2021 01:12:00 GMT'),
   },
-  {
-    id: 2,
-    title: 'Thoughts',
-    content: 'This and that',
-    lastEditTime: new Date('06 Oct 2021 05:24:32 GMT'),
-  },
-  {
-    id: 3,
-    title: 'Shopping List',
-    content: 'potatoes\n\nbeans\n\nflour',
-    lastEditTime: new Date('07 Oct 2021 10:11:03 GMT'),
-  },
-  {
-    id: 4,
-    title: '',
-    content: 'No title',
-    lastEditTime: new Date('08 Oct 2021 01:12:00 GMT'),
-  },
-  {
-    id: 5,
-    title: 'Earlier date than above, and no content',
-    content: '',
-    lastEditTime: new Date('03 Oct 2021 00:03:00 GMT'),
-  },
 ];
-
+ */
 export default function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState(null);
+  const [userInfo, setUserInfo] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [message, setMessage] = useState(null);
+  const [messageRed, setMessageRed] = useState(false);
+  const history = useHistory();
 
   useEffect(() => {
-    // TODO get todos from db
-    setTodoList(todos)
+    if (getToken) {
+      validateToken();
+    } else {
+      history.push("/login");
+    }
   }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setMessage(null);
+      setMessageRed(false);
+    }, 3000);
+  }, [message]);
+
+  const validateToken = () => {
+    FetchKit.getUserData()
+      .then(res => {
+        const { data } = res;
+        
+        if (!data) {
+          setUserInfo(null);
+          setIsLoggedIn(false);
+          setMessage('Session timed out, please sign in.');
+        } else {
+          setUserInfo(data);
+          setIsLoggedIn(true);
+        }
+      })
+  }
   
   return (
-    <>
-      <UserContext.Provider value={{todoList, setTodoList}}>
-        <Switch>
-            <Route path='/login' component={LoginPage} />
-            
-            <Route path='/todo/:id/edit' component={TodoEditPage} />
-            <Route path='/todo/:id' component={TodoDetailsPage} />
-            <Route path='/' component={HomePage} />
+    <UserContext.Provider
+      value={{
+        todoList, setTodoList,
+        isLoggedIn, setIsLoggedIn,
+        userInfo, setUserInfo,
+        history, setMessage}}
+    >
+      <Switch>
+          <Route path='/login' component={LoginPage} />
           
-          {/* //TODO 404 route without changing url 
-          https://github.com/remix-run/react-router/blob/v3/docs/guides/Histories.md#creatememoryhistory
-          */}
-        </Switch>
-      </UserContext.Provider>
-    </>
+          <Route path='/todo/:id/edit' component={TodoEditPage} />
+          <Route path='/todo/:id' component={TodoDetailsPage} />
+          <Route path='/' component={HomePage} />
+        
+        {/* //TODO 404 route without changing url 
+        https://github.com/remix-run/react-router/blob/v3/docs/guides/Histories.md#creatememoryhistory
+        */}
+      </Switch>
+    </UserContext.Provider>
   )
 }
