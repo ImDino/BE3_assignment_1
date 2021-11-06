347aa50fa93c075681f636ae9d8a022b955faa48
Dino Pacariz <dinopacariz@gmail.com>
2021-11-06 18:46:31 +0100

    expanded on fetchkit

--- client/src/data/FetchKit.js
+++ client/src/data/FetchKit.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-const serverLogin = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_ROOT,
-});
-
 export const getToken = () => {
   return localStorage.getItem('TOKEN_KEY');
 }
@@ -11,8 +7,22 @@ export const setToken = token => {
   localStorage.setItem('TOKEN_KEY', token);
 }
 
+const serverLogin = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_ROOT,
+});
+
+const serverDefault = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_ROOT,
+  headers: {
+    authorization: `Bearer ${getToken()}`
+  },
+})
+
 export const FetchKit = {
   login: googleToken => {
     return serverLogin.post('/auth/google', {tokenId: googleToken})
+  },
+  getUserData: () => {
+    return serverDefault.post('/user/getData')
   }
 }
