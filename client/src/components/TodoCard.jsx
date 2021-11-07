5005b5e0a188977cbb49212b5b94ff43831df6a0
Dino Pacariz <dinopacariz@gmail.com>
2021-11-07 18:46:13 +0100

    created delete button

--- client/src/components/TodoCard.jsx
+++ client/src/components/TodoCard.jsx
@@ -7,6 +7,23 @@ const Card = styled.div`
   padding: 7px;
   border-radius: 6px;
   white-space: nowrap;
+  position: relative;
+
+  &:not(:hover){
+    button {
+      display: none
+    }
+  }
+  
+  button {
+    height: 20px;
+    width: 20px;
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    border-radius: 50px;
+    padding-left: 0.8vw;
+  }
 `;
 
 export default function TodoCard({data}) {
@@ -17,8 +34,18 @@ export default function TodoCard({data}) {
     history.push(`/todo/${_id}`);
   };
 
+  function deleteTodo(e) {
+    e.stopPropagation();
+  }
+
   return (
     <Card onClick={openDetails}>
+      <button
+        type="button"
+        onClick={deleteTodo}
+      >
+        X
+      </button>
       <p>{title}</p>
     </Card>
   );
