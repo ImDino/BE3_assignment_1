a177714d40e525e8909df98c329fe6c20ea0b38d
Dino Pacariz <dinopacariz@gmail.com>
2021-11-07 22:37:37 +0100

    replacing template client id

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ export default function Navbar() {
             Home
           </button>
           <GoogleLogout
-            clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
             buttonText="Logout"
             onLogoutSuccess={kickUser}
           >
