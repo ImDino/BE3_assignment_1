50e9e3ac4c78c48fcf490f5d9175fcfe5350159f
Dino Pacariz <dinopacariz@gmail.com>
2021-11-09 20:38:39 +0100

    correction

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken');
 
 exports.authenticateToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+  const authHeader = req.headers && req.headers.authorization;
   const token = authHeader && authHeader.split(' ')[1];
   
-  if (token === null) return res.sendStatus(401);
+  if (token == null) return res.sendStatus(401);
   
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
